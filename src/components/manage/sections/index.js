2e69f85d7778be19783368dfe8dc8ad81b801eda
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-12-16 14:06:45 -0300

    Update index.js

--- src/components/manage/sections/index.js
+++ src/components/manage/sections/index.js
@@ -1,5 +1,6 @@
 // Exporta todos os componentes da pasta 'sections'
-export { EventInfo } from './event-info';
-export { EventTeam } from './event-team';
-export { RegistrationForm } from './registration-form';
+export { EventInfo } from './Event-info';
+export { EventTeam } from './Event-team';
+export { RegistrationForm } from './Registration-form';
+
 
